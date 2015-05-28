dcda2f1bc1eb65862796292719930ef5448ad564
John Newcomb <jnewcs@stanford.edu>
2015-05-28 10:14:07 -0700

    Changing gemfile, general setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
-//= require_tree .
+//= require jquery-readyselector
+$(function(){ $(document).foundation(); });
